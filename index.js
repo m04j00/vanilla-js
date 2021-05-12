a9b28decf2fada44ada21670ee8b661af1739f75
Lee MinJi <s2035@e-mirim.hs.kr>
2021-05-12 22:40:44 +0900

    ~1.10  2일차

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
+/*
+//1.5
 alert('Im Working. Im Js. Im pretty. Im worth it');
 console.log('Im Working. Im Js. Im pretty. Im worth it');
+*/
+/*
+//1.8
+//String
+const s = "Min";
+
+//Boolean
+const b = true;
+
+//Number
+const n = 666;
+
+//Float
+const f = 66.6;
+*/
+/*
+//1.9
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+console.log(daysOfWeek);
+*/
+
+//1.10
+const minInfo = {
+    name : "Minji",
+    age : 18,
+    gender : "female",
+    favMovies: ["Along the gods", "The Avengers"],
+    favFood : [
+        {
+            name : "Kimchi",
+            hot : true
+        },
+        {
+            name : "bulgogi",
+            hot : false
+        }
+    ]
+}
+
+console.log(minInfo.name);
+
+minInfo.name = "Min";
+
+console.log(minInfo.name);
+
+console.log(minInfo);
